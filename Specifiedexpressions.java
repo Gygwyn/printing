f3771365878dd40f91d329e59293f25fab82790f
Gygwyn <jacob.brorsson@elev.ga.ntig.se>
2018-09-14 09:44:33 +0200

    Specified Expressions

---
+++ Specifiedexpressions.java
@@ -0,0 +1,12 @@
+package printing;
+
+public class Specifiedexpressions {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Sum:" + ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));
+		
+		
+		
+	}
+}
