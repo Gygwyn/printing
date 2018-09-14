078dbd930469ef70579032e57f5b79304278ba84
Gygwyn <jacob.brorsson@elev.ga.ntig.se>
2018-09-14 09:49:24 +0200

    Specified Formula

---
+++ Specifiedformula.java
@@ -0,0 +1,12 @@
+package printing;
+
+public class Specifiedformula {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Sum:" +  (4.0 * (1 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11))));
+		
+
+
+	}
+}
