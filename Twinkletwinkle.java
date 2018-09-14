31ab2f9950887b5080d8955cc139e64cd697374d
Gygwyn <jacob.brorsson@elev.ga.ntig.se>
2018-09-14 09:23:17 +0200

    Twinkle Twinkle
    Little Star

---
+++ Twinkletwinkle.java
@@ -0,0 +1,17 @@
+package printing;
+
+public class Twinkletwinkle {
+	
+	public static void main(String[] args) {
+	
+		System.out.println("Twinkle, twinkle, litte star,");
+		System.out.println("How I wonder what you are!");
+		System.out.println("Up above the world so high,");
+		System.out.println("Like a diamond in the sky.");
+		System.out.println("Twinkle, twinkle, little star,");
+		System.out.println("How I wonder what you are");
+	
+	
+
+	}
+}
