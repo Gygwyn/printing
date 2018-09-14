1c7e86b3ffab932cde04d67187288a21252652ee
Gygwyn <jacob.brorsson@elev.ga.ntig.se>
2018-09-14 09:09:33 +0200

    Swedenflag
    Sverige.

---
+++ Swedenflag.java
@@ -0,0 +1,24 @@
+package printing;
+
+public class Swedenflag {
+
+	public static void main (String[] args) {
+
+		System.out.println("###########===################");
+		System.out.println("###########===################");
+		System.out.println("###########===################");
+		System.out.println("###########===################");
+		System.out.println("###########===################");
+		System.out.println("###########===################");
+		System.out.println("==============================");
+		System.out.println("==============================");
+		System.out.println("###########===################");
+		System.out.println("###########===################");
+		System.out.println("###########===################");
+		System.out.println("###########===################");
+		System.out.println("###########===################");
+		System.out.println("Sverige, i tiden.");
+	}
+
+	
+}
